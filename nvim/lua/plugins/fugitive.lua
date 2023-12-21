960c2a1dbef4d74f425f396f5af62bcd586c5059
robert-w <RWinter85@gmail.com>
2023-12-21 16:51:37 -0500

    feat: added shortcut for git diff of current file

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -4,7 +4,8 @@ return {
     init = function()
       vim.keymap.set('n', '<leader>gb', ':Git blame <CR>', { desc = 'Open git blame' });
       vim.keymap.set('n', '<leader>gs', ':Git status <CR>', { desc = 'Open git status' });
-      vim.keymap.set('n', '<leader>gl', ':Git log -- % <CR>', { desc = 'Open git log for current file' });
+      vim.keymap.set('n', '<leader>gl', ':Git log % <CR>', { desc = 'Open git log for current file' });
+      vim.keymap.set('n', '<leader>gd', ':Git diff % <CR>', { desc = 'Open git diff for current file' });
     end,
   }
 }
