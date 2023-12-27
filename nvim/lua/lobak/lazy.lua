fe06214409e94148f7dcf2838ebb2e89b4b6562b
Robert-W <robbie.winterbottom@gmail.com>
2023-12-27 09:16:01 -0500

    fix: updated initialization of lazy

--- nvim/lua/lobak/lazy.lua
+++ nvim/lua/lobak/lazy.lua
@@ -1,6 +1,8 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
+local uv = vim.uv or vim.loop
+
+if not uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
