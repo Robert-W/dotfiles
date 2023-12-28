54737e7ee516346d1754e0088f096d3b33ede5c4
Robert-W <robbie.winterbottom@gmail.com>
2023-12-28 00:19:02 -0500

    refactor: inlined single use requires

--- nvim/lua/config/lua_ls.lua
+++ nvim/lua/config/lua_ls.lua
@@ -1,11 +1,8 @@
-local lspcapabilities = require('cmp_nvim_lsp')
-local lspconfig = require('lspconfig')
-
 local M = {}
 
 function M.configure()
-  lspconfig.lua_ls.setup({
-    capabilities = lspcapabilities.default_capabilities(),
+  require('lspconfig').lua_ls.setup({
+    capabilities = require('cmp_nvim_lsp').default_capabilities(),
     settings = {
       Lua = {
         runtime = {
