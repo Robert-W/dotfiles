06296adfc602511fc26115b1bfe0c836c886b5f4
Robert Winterbottom <robbie.winterbottom@gmail.com>
2023-12-28 14:44:03 -0500

    chore: plugin organization and cleanup (#5)
    * refactor: remove drop plugin
    * refactor: removed colored highlights for indentation lines
    * refactor: removed cmp code from lsp into its own file
    * refactor: added config file for default lsp handler
    * refactor: removed code duplicated by previous refactorings
    * feat: updated colorscheme settings

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,17 @@ return {
   'catppuccin/nvim',
   priority = 1000,
   init = function()
-    vim.cmd('colorscheme catppuccin-mocha')
+    require('catppuccin').setup({
+      flavour = 'mocha',
+      integrations = {
+        aerial = true,
+        lsp_trouble = true,
+        mason = true,
+        notify = true,
+      },
+      transparent_background = true
+    })
+
+    vim.cmd('colorscheme catppuccin')
   end,
 }
--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -2,30 +2,7 @@ return {
   'lukas-reineke/indent-blankline.nvim',
   main = 'ibl',
   init = function()
-    local hooks = require('ibl.hooks')
-    -- Setup our highlight sequence
-    local highlight = {
-      'iYellow',
-      'iRed',
-      'iCyan',
-      'iRose',
-      'iGreen',
-      'iPurple'
-    }
-
-    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vim.api.nvim_set_hl(0, 'iYellow', { fg = '#F9D791' })
-      vim.api.nvim_set_hl(0, 'iRed', { fg = '#D27E99' })
-      vim.api.nvim_set_hl(0, 'iCyan', { fg = '#A3D4D5' })
-      vim.api.nvim_set_hl(0, 'iRose', { fg = '#D9A594' })
-      vim.api.nvim_set_hl(0, 'iGreen', { fg = '#6A9589' })
-      vim.api.nvim_set_hl(0, 'iPurple', { fg = '#766B90' })
-    end)
-
     require('ibl').setup({
-      indent = {
-        highlight = highlight
-      },
       scope = {
         enabled = true
       }
