f5716e4ba7d24269385133681ab0ea31ae57b9f1
Robert Winterbottom <robbie.winterbottom@gmail.com>
2023-12-30 17:44:15 -0500

    feat: various ui enhancements (#6)
    * feat: added borders to various lsp actions
    * feat: updated settings in colorscheme.lua
    * feat: added harpoon for jumping around to other files

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,13 +6,32 @@ return {
       flavour = 'mocha',
       integrations = {
         aerial = true,
+        harpoon = true,
         lsp_trouble = true,
         mason = true,
+        neotree = true,
         notify = true,
       },
-      transparent_background = true
+      transparent_background = true,
+      show_end_of_buffer = true,
+      styles = {
+        operators = { 'bold' },
+        types = { 'bold' },
+      }
     })
 
+    -- Set the colorscheme
     vim.cmd('colorscheme catppuccin')
+
+    -- Override some UI options, add borders to the following items
+    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+      vim.lsp.handlers.hover, { border = 'rounded' }
+    )
+
+    vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+      vim.lsp.handlers.signature_help, { border = 'rounded' }
+    )
+
+    vim.diagnostic.config({ float = { border = 'rounded' }})
   end,
 }
