f591c72f20ed9f81af14b5d2ab6bea9b5e8ae7e8
Robert-W <robbie.winterbottom@gmail.com>
2024-01-22 00:37:36 -0500

    feat: updated separators in lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,8 +4,8 @@ return {
     options = {
       icons_enabled = true,
       theme = 'catppuccin',
-      section_separators = { '', '' },
-      component_separators = { '', '' },
+      section_separators = { left = '', right = '' },
+      component_separators = { left = '|', right = '|' },
       disabled_filetypes = {}
     },
     sections = {
