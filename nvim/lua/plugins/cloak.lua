4a685644186e634e4ddb654cdc3bc037107a5950
robert-w <RWinter85@gmail.com>
2024-02-21 17:28:11 -0500

    feat: added cloak.nvim to hide credentials when screen sharing

---
+++ nvim/lua/plugins/cloak.lua
@@ -0,0 +1,20 @@
+return {
+  'laytan/cloak.nvim',
+  config = function()
+    require('cloak').setup({
+      enabled = false,
+      patterns = {
+        {
+          file_pattern = {
+            '.env*',
+          },
+          cloak_pattern = '=.+'
+        }
+      },
+    })
+
+    -- Setup shortcut to toggle cloak on and off
+    vim.keymap.set('n', '<leader>sh', '<cmd>CloakToggle<CR>', { desc = 'Toggle secret values with Cloak' })
+
+  end
+}
