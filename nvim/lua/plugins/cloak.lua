9dffebecd504658f5a12ca2ee5f14bfae0c0615d
robert-w <RWinter85@gmail.com>
2024-04-02 15:19:19 -0400

    feat: added additional files to cloak

--- nvim/lua/plugins/cloak.lua
+++ nvim/lua/plugins/cloak.lua
@@ -5,7 +5,10 @@ return {
       enabled = true,
       patterns = {
         {
-          file_pattern = '.env*',
+          file_pattern = {
+            '.env*',
+            '*.env'
+          },
           cloak_pattern = '=.+'
         },
         {
