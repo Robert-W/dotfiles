f96ac973831438a38dc2881a0109b1aa7350e067
robert-w <RWinter85@gmail.com>
2024-06-17 10:57:21 -0400

    feat: updated plugins/trouble

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,13 +1,20 @@
 return {
   'folke/trouble.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  cmd = 'Trouble',
+  keys = {
+    {
+      '<leader>dw',
+      '<cmd>Trouble diagnostics toggle<cr>',
+      desc = '[D]iagnostics for [W]orkspace'
+    },
+    {
+      '<leader>df',
+      '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+      desc = '[D]iagnostics for [F]ile'
+    },
+  },
   init = function()
-    local trouble = require('trouble')
-
-    vim.keymap.set('n', '<leader>dw', function() trouble.open('workspace_diagnostics') end,
-      { desc = '[D]iagnostics for [W]orkspace' })
-    vim.keymap.set('n', '<leader>df', function() trouble.open('document_diagnostics') end,
-      { desc = '[D]iagnostics for [F]ile' })
     vim.keymap.set('n', '<leader>dl', vim.diagnostic.open_float,
       { desc = '[D]iagnostics for [L]ine' })
   end,
