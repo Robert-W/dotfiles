3a90e9a0dcaf9a438e7570d6db08c004d0bce512
robert-w <RWinter85@gmail.com>
2024-09-05 15:20:27 -0400

    fix: fixed tsserver inlay hints configurations

--- nvim/lua/config/tsserver.lua
+++ nvim/lua/config/tsserver.lua
@@ -1,25 +1,18 @@
 local M = {}
 
-local inlay_hints = {
-  includeInlayEnumMemberValueHints = true,
-  includeInlayFunctionLikeReturnTypeHints = true,
-  includeInlayFunctionParameterTypeHints = true,
-  includeInlayParameterNameHints = "all",
-  includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-  includeInlayPropertyDeclarationTypeHints = true,
-  includeInlayVariableTypeHints = false,
-}
-
 function M.configure(capabilities)
   return function()
     require('lspconfig').tsserver.setup({
       capabilities = capabilities,
-      settings = {
-        javascript = {
-          inlay_hints = inlay_hints
-        },
-        typescript = {
-          inlay_hints = inlay_hints
+      init_options = {
+        preferences = {
+          includeInlayEnumMemberValueHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayParameterNameHints = "all",
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayVariableTypeHints = true,
         }
       }
     })
