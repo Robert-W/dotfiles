4a88bc73a8dec229c7d37f9baac0716fedfbdb7b
Robert Winterbottom <robertwinterbottom@robertwinterbottom.fios-router.home>
2025-12-03 22:14:43 -0500

    chore: mode quotes consistent

--- nvim/lua/lobak/autocmd.lua
+++ nvim/lua/lobak/autocmd.lua
@@ -2,25 +2,25 @@ local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
 local TrimWhitespaceGroup = augroup('TrimWhitespace', {})
-local TextFormatGroup = augroup("TextFormat", { clear = true })
-local TextYankGroup = augroup("TextYank", { clear = true })
+local TextFormatGroup = augroup('TextFormat', { clear = true })
+local TextYankGroup = augroup('TextYank', { clear = true })
 
 -- Trim whitespace when saving files
-autocmd({ "BufWritePre" }, {
+autocmd({ 'BufWritePre' }, {
   group = TrimWhitespaceGroup,
-  pattern = "*",
+  pattern = '*',
   command = [[%s/\s\+$//e]],
 })
 
 -- Allow code to exceed vim.opt.textwidth
-autocmd({ "BufWinEnter" }, {
+autocmd({ 'BufWinEnter' }, {
   group = TextFormatGroup,
   buffer = 0,
-  command = "set formatoptions-=t"
+  command = 'set formatoptions-=t'
 })
 
 -- Highlight text that is being yanked
-autocmd({ "TextYankPost" }, {
+autocmd({ 'TextYankPost' }, {
   group = TextYankGroup,
   callback = function ()
     vim.highlight.on_yank()
