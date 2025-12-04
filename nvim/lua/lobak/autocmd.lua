cdbfae6278fa4f239274de89305a787c9f67de17
Robert Winterbottom <robbie.winterbottom@gmail.com>
2025-12-03 22:24:54 -0500

    feat: change default fold settings (#15)

--- nvim/lua/lobak/autocmd.lua
+++ nvim/lua/lobak/autocmd.lua
@@ -2,25 +2,34 @@ local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
 local TrimWhitespaceGroup = augroup('TrimWhitespace', {})
-local TextFormatGroup = augroup("TextFormat", { clear = true })
-local TextYankGroup = augroup("TextYank", { clear = true })
+local TextFormatGroup = augroup('TextFormat', { clear = true })
+local TextYankGroup = augroup('TextYank', { clear = true })
+local FoldingGroup = augroup('CodeFold', { clear = true })
 
 -- Trim whitespace when saving files
-autocmd({ "BufWritePre" }, {
+autocmd({ 'BufWritePre' }, {
   group = TrimWhitespaceGroup,
-  pattern = "*",
+  pattern = '*',
   command = [[%s/\s\+$//e]],
 })
 
 -- Allow code to exceed vim.opt.textwidth
-autocmd({ "BufWinEnter" }, {
+autocmd({ 'BufWinEnter' }, {
   group = TextFormatGroup,
   buffer = 0,
-  command = "set formatoptions-=t"
+  command = 'set formatoptions-=t'
+})
+
+-- Allow code to exceed vim.opt.textwidth
+autocmd({ 'BufWinEnter' }, {
+  group = FoldingGroup,
+  callback = function ()
+    vim.cmd('normal zR')
+  end
 })
 
 -- Highlight text that is being yanked
-autocmd({ "TextYankPost" }, {
+autocmd({ 'TextYankPost' }, {
   group = TextYankGroup,
   callback = function ()
     vim.highlight.on_yank()
