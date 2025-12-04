6944ab33c45494b8fb46f4abddeee66968192523
Robert Winterbottom <robertwinterbottom@robertwinterbottom.fios-router.home>
2025-12-04 10:04:16 -0500

    fix: remove autocmd to run zR as it interferes with telescope and doesn't work in all situation

--- nvim/lua/lobak/autocmd.lua
+++ nvim/lua/lobak/autocmd.lua
@@ -4,7 +4,6 @@ local augroup = vim.api.nvim_create_augroup
 local TrimWhitespaceGroup = augroup('TrimWhitespace', {})
 local TextFormatGroup = augroup('TextFormat', { clear = true })
 local TextYankGroup = augroup('TextYank', { clear = true })
-local FoldingGroup = augroup('CodeFold', { clear = true })
 
 -- Trim whitespace when saving files
 autocmd({ 'BufWritePre' }, {
@@ -18,14 +17,6 @@ autocmd({ 'BufWinEnter' }, {
   group = TextFormatGroup,
   buffer = 0,
   command = 'set formatoptions-=t'
-})
-
--- Allow code to exceed vim.opt.textwidth
-autocmd({ 'BufWinEnter' }, {
-  group = FoldingGroup,
-  callback = function ()
-    vim.cmd('normal zR')
-  end
 })
 
 -- Highlight text that is being yanked
