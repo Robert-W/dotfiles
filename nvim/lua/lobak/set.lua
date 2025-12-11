0b84e8df93d698ca496c2e811ead697e0d61b97e
Robert Winterbottom <robertwinterbottom@robertwinterbottom.fios-router.home>
2025-12-11 00:06:41 -0500

    feat: set cmdheight to 0, showmatch to true, and organized/commented on settings

--- nvim/lua/lobak/set.lua
+++ nvim/lua/lobak/set.lua
@@ -21,25 +21,21 @@ vim.opt.backup = false
 vim.opt.undofile = true
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 
+-- Command line search options
+vim.opt.cmdheight = 0
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
 
+-- Editor options
 vim.opt.title = true
 vim.opt.ruler = true
 vim.opt.showcmd = true
+vim.opt.showmatch = true
 vim.opt.errorbells = false
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- Convert tabs to 2 spaces on save
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-
-vim.opt.cmdheight = 1
 vim.opt.scrolloff = 8
 vim.opt.textwidth = 80
 vim.opt.signcolumn = "yes"
@@ -48,3 +44,10 @@ vim.opt.termguicolors = true
 
 vim.opt.foldlevel = 99
 vim.opt.foldmethod = 'indent'
+
+-- Convert tabs to 2 spaces on save
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
