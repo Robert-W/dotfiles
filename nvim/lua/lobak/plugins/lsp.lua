c4cfdd4577f93dc4162650bb0c4abd2c6b68dcf0
Robert Winterbottom <robbie.winterbottom@gmail.com>
2026-01-15 23:48:18 -0500

    feat: cleanup repo (#24)

--- nvim/lua/lobak/plugins/lsp.lua
+++ nvim/lua/lobak/plugins/lsp.lua
@@ -13,30 +13,46 @@ return {
     },
     config = function()
       local masoncfg = require('mason-lspconfig')
+      local blink = require('blink.cmp')
+
+      local servers = {
+        'bashls',
+        'cssls',
+        'elixirls',
+        'eslint',
+        'gopls',
+        'html',
+        'jsonls',
+        'lua_ls',
+        'rust_analyzer',
+        'sqlls',
+        'terraformls',
+        'ts_ls',
+      }
+
+      local capabilities = vim.tbl_deep_extend(
+        'force',
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        blink.get_lsp_capabilities()
+      )
+
+      -- Configure the LSP
+      for _, server_name in pairs(servers) do
+        vim.lsp.config(server_name, { capabilities = capabilities })
+        vim.lsp.enable(server_name)
+      end
 
       -- Mason LSP Config
       masoncfg.setup({
-        automatic_enable = true,
-        ensure_installed = {
-          'bashls',
-          'cssls',
-          'elixirls',
-          'eslint',
-          'gopls',
-          'html',
-          'jsonls',
-          'lua_ls',
-          'rust_analyzer',
-          'sqlls',
-          'terraformls',
-          'ts_ls',
-        },
+        automatic_enable = false,
+        ensure_installed = servers,
       })
 
       -- Install packages masoncfg cannot, these include linters,
       -- formatters, and DAP packages. These do require additional setup, so
       -- these alone do nothing
-      require('utils.mason-packager').setup({
+      require('lobak.utils.mason-packager').setup({
         ensure_installed = {
           'codelldb',
         }
@@ -45,16 +61,15 @@ return {
       -- Use this to setup all of our keybindings when a server attaches
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(event)
-          local builtin = require('telescope.builtin')
           -- Helper function for keymaps
           local map = function(mode, keys, func, desc)
             vim.keymap.set(mode, keys, func, { buffer = event.buf, remap = false, desc = 'LSP: ' .. desc })
           end
 
-          map('n', 'gD', builtin.lsp_type_definitions, '[G]oto Type [D]efinition')
-          map('n', 'gI', builtin.lsp_implementations, '[G]oto [I]mplementation')
-          map('n', 'gd', builtin.lsp_definitions, '[G]oto [D]efinition')
-          map('n', 'gr', builtin.lsp_references, '[G]oto [R]eferences')
+          map('n', 'gD', vim.lsp.buf.type_definition, '[G]oto Type [D]efinition')
+          map('n', 'gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+          map('n', 'gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+          map('n', 'gr', vim.lsp.buf.references, '[G]oto [R]eferences')
           map('n', 'gp', function() vim.lsp.buf.format({ timeout_ms = 10000 }) end, '[G][P] Format current buffer')
           map('n', 'gk', function() vim.lsp.buf.hover({ border = 'rounded' }) end, '[G][K] Hover documentation')
           map('n', '<leader>r', vim.lsp.buf.rename, '[R]ename variable')
@@ -63,11 +78,6 @@ return {
 
           -- Enable inlay_hints for the active buffer
           vim.lsp.inlay_hint.enable(true)
-
-          -- vim.keymap.set('n', 'gc', vim.lsp.buf.incoming_calls,
-          --   { buffer = bufnr, remap = false, desc = 'Show call sites of the symbol' })
-          -- vim.keymap.set('n', 'go', vim.lsp.buf.outgoing_calls,
-          --   { buffer = bufnr, remap = false, desc = 'Show items called by the symbol' })
         end
       })
 
