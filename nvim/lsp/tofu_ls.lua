e53d4f199dc65af10045e6b6f69dbc8d9bdb9337
Robert Winterbottom <robbie.winterbottom@gmail.com>
2026-02-17 16:57:55 -0500

    fix: issue with neovim not picking up the filetype on empty/new terraform files (#28)
    Co-authored-by: Robert Winterbottom <robertwinterbottom@robertwinterbottom.fios-router.home>

--- nvim/lsp/tofu_ls.lua
+++ nvim/lsp/tofu_ls.lua
@@ -1,5 +1,13 @@
+-- There is some strangeness around detecting filetypes, use this to enforce it
+vim.filetype.add({
+  extension = {
+    tf = 'opentofu',
+    tofu = 'opentofu',
+  }
+})
+
 return {
   cmd = { 'tofu-ls', 'serve' },
-  filetypes = { 'terraform', 'terraform-vars', 'tf' },
+  filetypes = { 'terraform', 'terraform-vars', 'tf', 'opentofu', 'opentofu-vars' },
   root_markers = { '.terraform', '.git' },
 }
