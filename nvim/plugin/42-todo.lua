a034d10fbfa896dc24a27428c046c0f77f066343
Robert Winterbottom <robbie.winterbottom@gmail.com>
2026-04-03 14:15:40 -0400

    feat: cleanup

--- nvim/plugin/42-todo.lua
+++ nvim/plugin/42-todo.lua
@@ -1,9 +1,7 @@
 vim.api.nvim_create_autocmd('VimEnter', {
   once = true,
   callback = function()
-    vim.pack.add({
-      'https://github.com/folke/todo-comments.nvim',
-    })
+    vim.pack.add({ 'https://github.com/folke/todo-comments.nvim' })
     require('todo-comments').setup({ signs = false })
   end
 })
